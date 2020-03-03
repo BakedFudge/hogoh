b6cfbc9727b48a34113e5a2f387ceaa3704fd82e
Aswin <letsgetfudged@gmail.com>
2020-03-03 09:40:58 +0530

    CURD Incomplete

---
+++ lib/Models/doUserProfile.dart
@@ -0,0 +1,17 @@
+
+
+class doUserProfile{
+
+  final String name;
+  final String tag;
+  final String about;
+  final int age;
+//  final String city;
+//  final String gender;
+//  final String veg_nv;
+//  final String fans;
+//  final double level;
+
+  doUserProfile({this.name,this.tag,this.about,this.age});
+
+}
